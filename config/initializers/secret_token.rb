cc47cf5613e15676449fda669ded1b4e7de430bc
johonokaz <jhonokaz@gmail.com>
2014-06-14 16:15:32 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwiCamo::Application.config.secret_key_base = 'cebb2c628c78e0b8141b364ac9ee1f0c181ff721ceccc5f907c192abf26b215385126be8b3b320b7862a5c4ccc61a2acae1ad06f7c385ad4500313de1cd8003e'
